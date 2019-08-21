f00809d6cb95669a9a1e6ecab1efe84b63b5a0c9
olzzon <github@olzzon.dk>
2019-08-21 21:10:59 +0200

    added example_DEFAULT.ts as a Temporary DEFAULTS.ts file is required until the correct CORE integration is made.

---
+++ src/example_DEFAULTS.ts
@@ -0,0 +1,6 @@
+/* This file is temporary until CORE integration is setup correctly */
+
+export const SERVERS = ['xx.xx.xx.xx','yy.yy.yy.yy']
+export const USERNAME = 'iNewsUserName'
+export const PASSWORD = 'iNewsPassword'
+export const INEWS_QUEUE = 'INEWS.QUEUE.ON-AIR'

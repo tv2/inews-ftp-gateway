c3229380eaec9883ec06d9a0a2ccf60988983379
olzzon <github@olzzon.dk>
2019-08-28 08:25:04 +0200

    refactore: forgot to add moved files to git

---
+++ src/classes/manusConverters/ManusTypeEmpty.ts
@@ -0,0 +1,32 @@
+import { IParsedElement } from '../Rundown'
+import { IAeCodes } from '../converters/AeCodesToJs'
+
+export class ManusTypeEmpty {
+
+	static convert (convertedStory: any, script: string, aeCodes: IAeCodes[]): IParsedElement[] {
+		console.log('DUMMY LOG :', aeCodes)
+
+		let elements: IParsedElement[] = []
+		const f = convertedStory.root.story[0].fields[0].f
+
+		let name = f[f.findIndex((x: any) => x.id[0] === 'title')]._
+		elements.push({
+			data: {
+				id: convertedStory.root.head[0].storyid + 'camera',
+				name: name,
+				type: 'CAM',
+				float: 'false',
+				script: script,
+				objectType: 'camera',
+				objectTime: '0',
+				duration: '10',
+				clipName: 'string',
+				feedback: 'string',
+				transition: 'string',
+				attributes: { ['Name']: 'CAM1' }
+			}
+		})
+
+		return elements
+	}
+}

1e3ee6388f72038dca22cf86f9c0bfcf5601cf41
Tom Lee <thomasslee97@gmail.com>
2019-09-27 10:09:43 +0100

    chore: Tests

--- src/classes/__tests__/RunningOrders.spec.ts
+++ src/classes/__tests__/RunningOrders.spec.ts
@@ -1,16 +1,9 @@
 let fs = require('fs')
 let ftpData = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/ftpData.json'))
-let parsedBodyCodes = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/parsedData_BodyCodes.json'))
-let parsedCues = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/parsedData_Cues.json'))
-let parsedElements = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/parsedData_Elements.json'))
-let parsedFields = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/parsedData_Fields.json'))
-let parsedMeta = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/parsedData_Meta.json'))
-let parsedSegments = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/segments.json'))
+let parsedData = JSON.parse(fs.readFileSync('src/classes/__tests__/__mocks__/parsedData.json'))
 import { InewsRundown } from '../datastructures/Rundown'
 import winston = require('winston')
-import { IOutputLayer } from 'tv-automation-sofie-blueprints-integration'
-import { SplitRawDataToElements } from '../converters/SplitRawDataToElements'
-import { ParsedElementsIntoSegments } from '../ParsedElementsToSegments'
+import { RundownManager } from '../RundownManager'
 
 describe('RunningOrders', () => {
 
@@ -20,42 +13,12 @@ describe('RunningOrders', () => {
 	})
 })
 
-let outputLayers: IOutputLayer[] = [
-	{
-		_id: 'pgm0',
-		name: 'Program',
-		_rank: 1,
-		isPGM: true
-	}
-]
-
 describe('RundownManager', () => {
 	it('SplitRawDataToElements', () => {
-		let logger = new winston.Logger()
-		let parsedData = SplitRawDataToElements.convert(logger, JSON.parse(JSON.stringify(ftpData)), outputLayers)
-		expect(parsedData.meta).toEqual(parsedMeta)
-		expect(parsedData.fields).toEqual(parsedFields)
-		expect(parsedData.bodyCodes).toEqual(parsedBodyCodes)
-		expect(parsedData.cues).toEqual(parsedCues)
-		expect(parsedData.elements).toEqual(parsedElements)
-	})
-	it('INewsRundown', () => {
 		let id = '00000000000001'
 		let logger = new winston.Logger()
-		let parsedData = SplitRawDataToElements.convert(logger, JSON.parse(JSON.stringify(ftpData)), outputLayers)
-		let rundown = new InewsRundown(id, id, parsedData.meta.version, parsedData.meta.startTime, parsedData.meta.endTime)
-		expect(rundown.externalId).toEqual(id)
-		expect(rundown.name).toEqual(id)
-		expect(rundown.gatewayVersion).toEqual('v0.2')
-		expect(rundown.expectedStart).toEqual(0)
-		expect(rundown.expectedEnd).toEqual(1)
-		expect(rundown.segments).toEqual([])
-	})
-	it('Segments', () => {
-		let id = '00000000000001'
-		let logger = new winston.Logger()
-		let parsedData = SplitRawDataToElements.convert(logger, JSON.parse(JSON.stringify(ftpData)), outputLayers)
-		let segments = ParsedElementsIntoSegments.parse(id, parsedData.elements, parsedData.fields, parsedData.bodyCodes, parsedData.cues)
-		expect(segments).toEqual(parsedSegments)
+		let manager = new RundownManager(logger, {})
+		let managerParsedData = manager.convertRawtoSofie(logger, id, id, JSON.parse(JSON.stringify(ftpData)))
+		expect(managerParsedData).toEqual(parsedData)
 	})
 })

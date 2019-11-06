db6971473d5ddd5c48311e30df7fae69af3c98dc
Tom Lee <thomasslee97@gmail.com>
2019-11-06 21:50:10 +0000

    fix: Only clear jobs in main loop

--- src/coreHandler.ts
+++ src/coreHandler.ts
@@ -352,11 +352,6 @@ export class CoreHandler {
 			if (this.iNewsHandler.iNewsWatcher) {
 				const rundown = this.iNewsHandler.iNewsWatcher.DownloadRunningOrderById(roId)
 				.then((ro) => {
-					if (this.iNewsHandler) {
-						if (this.iNewsHandler.iNewsWatcher) {
-							this.iNewsHandler.iNewsWatcher.rundownManager.EmptyInewsFtpBuffer()
-						}
-					}
 					return mutateRundown(ro)
 				})
 				.catch((err) => {

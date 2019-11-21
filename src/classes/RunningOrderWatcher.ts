1022d5cd272cfabe386e2ede911e64f34885dfe6
Richard Cartwright <spark@streampunk.media>
2019-11-21 10:27:37 +0000

    fix: remove queueLock due to improved parallel handling

--- src/classes/RunningOrderWatcher.ts
+++ src/classes/RunningOrderWatcher.ts
@@ -98,7 +98,10 @@ export class RunningOrderWatcher extends EventEmitter {
 			this.currentlyChecking = true
 
 			this.checkINewsRundowns().then(() => {
-				this.rundownManager.EmptyInewsFtpBuffer()
+				// this.rundownManager.EmptyInewsFtpBuffer()
+				if (this.iNewsConnection.queueLength() > 0) {
+					this.logger.error(`INews library queue length was ${this.iNewsConnection.queueLength()} when it should be 0.`)
+				}
 				// console.log('slow check done')
 				this.currentlyChecking = false
 				return this.coreHandler.setStatus(P.StatusCode.GOOD, [`Watching iNews Queues`])

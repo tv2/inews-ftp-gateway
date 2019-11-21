2167127bbf053b4c7b71792b2d24210aa4021bf6
Richard Cartwright <spark@streampunk.media>
2019-11-21 12:36:30 +0000

    chore: hide _queue again

--- src/@types/@johnsand/inews/index.d.ts
+++ src/@types/@johnsand/inews/index.d.ts
@@ -40,10 +40,10 @@ declare module '@johnsand/inews' {
 		type status = 'connecting' | 'connected' | 'error' | 'disconnected'
 
 		interface INewsClient extends EventEmitter {
-			_queue: { // Expose queue so if can be flsuhed after used
+			/* _queue: { // Expose queue so if can be flsuhed after used
 				queuedJobList: { list: object },
 				inprogressJobList: { list: object }
-			}
+			} */
 			list (queneName: string, cb: (error: Error | null, dirList: Array<INewsDirItem>) => void): void
 			story (queueName: string, file: string, cb: (error: Error | null, rawStory: INewsStory) => void): void
 			storyNsml (queueName: string, file: string, cb: (error: Error | null, nsml: string) => void): void

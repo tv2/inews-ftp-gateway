91cef589a29a071fb1e300505a9d874b5a05c4c1
Tom Lee <thomasslee97@gmail.com>
2021-05-20 17:32:50 +0100

    fix: Exclude modified time from serialization

--- src/classes/datastructures/Segment.ts
+++ src/classes/datastructures/Segment.ts
@@ -32,7 +32,7 @@ export class RundownSegment implements ISegment {
 	serialize(): Omit<ISegment, 'modified' | 'locator'> {
 		return {
 			rundownId: this.rundownId,
-			iNewsStory: this.iNewsStory,
+			iNewsStory: { ...this.iNewsStory, fields: { ...this.iNewsStory.fields, modifyDate: '' } },
 			externalId: this.externalId,
 			rank: this.rank,
 			name: this.name,

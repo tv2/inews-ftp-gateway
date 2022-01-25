c7dcc464c82938674f3d73125dc44bdca03cd250
Anders Frederik Jørgensen <afjo@tv2.dk>
2022-01-25 12:59:12 +0100

    wip: Missed linting (sorted graphic profiles).

--- src/helpers/ResolveRundownIntoPlaylist.ts
+++ src/helpers/ResolveRundownIntoPlaylist.ts
@@ -87,24 +87,26 @@ function shouldLookForGraphicProfile(segment: UnrankedSegment, rundown: Resolved
 	return !isFloated && isKlarOnAirSegment && !hasGraphicProfile
 }
 /**
- * 
+ *
  * @param segment Segment of which graphic profile cues are to be extracted.
  * @returns A sorted list of graphic profiles for the given segment.
  */
 function extractGraphicProfiles(segment: UnrankedSegment): string[] {
-
 	// Extract Graphic Profiles
-	const graphicProfiles = segment.iNewsStory.cues.reduce<{ cueIndex: number, graphicProfile: string }[]>((graphicProfiles: { cueIndex: number, graphicProfile: string }[], cue: UnparsedCue, cueIndex: number) => {
-		const numberOfCueLines = cue !== null ? cue.length : -1
+	const graphicProfiles = segment.iNewsStory.cues.reduce<{ cueIndex: number; graphicProfile: string }[]>(
+		(graphicProfiles: { cueIndex: number; graphicProfile: string }[], cue: UnparsedCue, cueIndex: number) => {
+			const numberOfCueLines = cue !== null ? cue.length : -1
 
-		// Kommando cue (ignoring timing)
-		const kommandoPattern = /^\s*KOMMANDO\s*=\s*GRAPHICSPROFILE/i
-		if (numberOfCueLines >= 2 && kommandoPattern.test(cue![0])) {
-			const graphicProfile = cue![1].trim()
-			return [...graphicProfiles, { cueIndex, graphicProfile }]
-		}
-		return graphicProfiles
-	}, [])
+			// Kommando cue (ignoring timing)
+			const kommandoPattern = /^\s*KOMMANDO\s*=\s*GRAPHICSPROFILE/i
+			if (numberOfCueLines >= 2 && kommandoPattern.test(cue![0])) {
+				const graphicProfile = cue![1].trim()
+				return [...graphicProfiles, { cueIndex, graphicProfile }]
+			}
+			return graphicProfiles
+		},
+		[]
+	)
 
 	// Sort by cue order
 	const cueOrder = getCueOrder(segment)
@@ -118,8 +120,9 @@ function extractGraphicProfiles(segment: UnrankedSegment): string[] {
 
 function getCueOrder(segment: UnrankedSegment): number[] {
 	const body = (segment.iNewsStory.body ?? '').split('\n')
-	const order = body.map(line => line.match(/<a\s+idref="(?<id>\d+)"\s+\/>/i)?.groups?.id)
-		.map(id => parseInt(id ?? '', 10))
-		.filter(id => !isNaN(id))
+	const order = body
+		.map((line) => line.match(/<a\s+idref="(?<id>\d+)"\s+\/>/i)?.groups?.id)
+		.map((id) => parseInt(id ?? '', 10))
+		.filter((id) => !isNaN(id))
 	return order
 }

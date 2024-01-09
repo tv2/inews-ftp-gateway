a16a68950283288d0b5f923ef2ff61a165f3f112
Anders Frederik Jørgensen <afjo@tv2.dk>
2024-01-09 13:01:53 +0100

    fix: Showstyle variant parsing reintroduced.
    Only one showstyle variant per rundown/queue is supported. The first encountered will be used.

--- src/helpers/ResolveRundownIntoPlaylist.ts
+++ src/helpers/ResolveRundownIntoPlaylist.ts
@@ -28,6 +28,13 @@ export function ResolveRundownIntoPlaylist(
 	let klarOnAirStoryFound = false
 
 	for (const segment of segments) {
+		if (shouldLookForShowstyleVariant(segment, resolvedRundown)) {
+			const showstyleVariantsForSegment = getOrderedShowstyleVariants(segment)
+			if (showstyleVariantsForSegment.length > 0) {
+				setShowstyleVariant(resolvedRundown, showstyleVariantsForSegment[0])
+			}
+		}
+
 		resolvedRundown.segments.push(segment.externalId)
 
 		const isFloated = segment.iNewsStory.meta.float ?? false
@@ -49,6 +56,59 @@ export function ResolveRundownIntoPlaylist(
 	}
 
 	return { resolvedRundown, untimedSegments }
+}
+
+function shouldLookForShowstyleVariant(segment: UnrankedSegment, rundown: ResolvedPlaylistRundown): boolean {
+	const isFloated: boolean = !!segment.iNewsStory.meta.float ?? false
+	const hasShowstyleVariant: boolean = rundown.payload?.showstyleVariant !== undefined
+	return !isFloated && !hasShowstyleVariant
+}
+
+function getOrderedShowstyleVariants(segment: UnrankedSegment): string[] {
+	const cueOrder: number[] = getCueOrder(segment)
+	const orderedShowstyleVariants: string[] = []
+	cueOrder.forEach((cueIndex: number) => {
+		const parsedProfile: string | null = parseShowstyleVariant(segment.iNewsStory.cues[cueIndex] ?? [])
+		if (parsedProfile) {
+			orderedShowstyleVariants.push(parsedProfile)
+		}
+	})
+	return orderedShowstyleVariants
+}
+
+function parseShowstyleVariant(cue: string[]): string | null {
+	const numberOfCueLines: number = cue.length
+
+	// Kommando cue (ignoring timing)
+	const showstyleVariantPattern: RegExp = /^\s*SOFIE\s*=\s*SHOWSTYLEVARIANT/i
+	if (numberOfCueLines >= 2 && showstyleVariantPattern.test(cue![0])) {
+		return cue![1].trim()
+	}
+	return null
+}
+
+/**
+ *
+ * @param segment The segment for which the cue order should be retrieved
+ * @returns A list of indicies representing the cue order.
+ */
+function getCueOrder(segment: UnrankedSegment): number[] {
+	const body = segment.iNewsStory.body ?? ''
+	const refPattern = /<a\s+idref="(?<id>\d+)"\s*\/?>/gi
+	const order: number[] = []
+	let match: RegExpExecArray | null
+	while ((match = refPattern.exec(body))) {
+		let id = parseInt(match.groups!.id, 10)
+		order.push(id)
+	}
+	return order
+}
+
+function setShowstyleVariant(rundown: ResolvedPlaylistRundown, showstyleVariant: string) {
+	rundown.payload = {
+		...(rundown.payload ?? null),
+		showstyleVariant,
+	}
 }
 
 function isKlarOnAir(segment: UnrankedSegment): boolean {
